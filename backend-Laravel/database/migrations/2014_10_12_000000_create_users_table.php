19fbc0ca470dec99d1892457b4aeea042e07dc9e
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-04 23:10:45 +0300

    laravel | Created users Table

--- backend-Laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend-Laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,9 +17,17 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('address')->nullable();
+            $table->string('plate_number')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->integer('type')->default(0);
+            $table->integer('is_approved')->default(0);
+            $table->foreignId('photo_id')->default(0);
+            $table->foreignId('city_id')->default(0);
             $table->rememberToken();
+
+            
             $table->timestamps();
         });
     }
