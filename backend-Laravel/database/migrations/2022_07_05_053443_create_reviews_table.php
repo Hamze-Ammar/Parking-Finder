0c3a7c65c082e495548f897c6dc7715589d5276e
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-05 08:41:20 +0300

    Laravel | Created Review Model

---
+++ backend-Laravel/database/migrations/2022_07_05_053443_create_reviews_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->text('context')->nullable();
+            $table->tinyInteger('rate');
+            $table->foreignId('user_id');
+            $table->foreignId('parking_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
