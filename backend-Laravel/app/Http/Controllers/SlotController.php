cba8baa2ff198cf2e1225ae11e34cb97cec2996e
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-05 08:48:23 +0300

    Laravel | Creaded Slot Model

---
+++ backend-Laravel/app/Http/Controllers/SlotController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Slot;
+use Illuminate\Http\Request;
+
+class SlotController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Slot  $slot
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Slot $slot)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Slot  $slot
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Slot $slot)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Slot  $slot
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Slot $slot)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Slot  $slot
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Slot $slot)
+    {
+        //
+    }
+}
---
+++ backend-Laravel/database/migrations/2022_07_05_054157_create_slots_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSlotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('slots', function (Blueprint $table) {
+            $table->id();
+            $table->integer('number');
+            $table->integer('is_available')->default(1);
+            $table->integer('is_reserved')->default(0);
+            $table->integer('parking_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('slots');
+    }
+}
