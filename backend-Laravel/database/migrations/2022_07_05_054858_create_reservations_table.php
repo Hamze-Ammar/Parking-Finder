902dc7bd34c86dcd69f428a36a5335c7fd9e3a04
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-05 08:54:33 +0300

    Laravel | Created Reservation Model

---
+++ backend-Laravel/database/migrations/2022_07_05_054858_create_reservations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->integer('is_valid')->default(1);
+            $table->integer('is_permanent')->default(0);
+            $table->foreignId('user_id');
+            $table->foreignId('slot_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
