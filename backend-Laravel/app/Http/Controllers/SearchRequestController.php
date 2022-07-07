116b2c5d04a9b8198f5516a9d91915a5df136abc
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-07 13:15:35 +0300

    Laravel | Create SearchRequest Controller

---
+++ backend-Laravel/app/Http/Controllers/SearchRequestController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SearchRequest;
+use Illuminate\Http\Request;
+
+class SearchRequestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\SearchRequest  $searchRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SearchRequest $searchRequest)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\SearchRequest  $searchRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SearchRequest $searchRequest)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\SearchRequest  $searchRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SearchRequest $searchRequest)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\SearchRequest  $searchRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SearchRequest $searchRequest)
+    {
+        //
+    }
+}
