d41bbb31ead8ee8d61381712f4283ade1244b37d
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-10 18:20:41 +0300

    ReactNative: Set up Colors

---
+++ frontend/ReactNative/constants/styles.js
@@ -0,0 +1,8 @@
+export const Colors = {
+    primary500: '#7862FF',
+    secondary500: '#1B3773',
+    background500: '#D9D9D9',
+    error100: '#F34913',
+    error200: '#F34913',
+    error500: '#F34913',
+  }
