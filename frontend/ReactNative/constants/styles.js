7c48e91a94d049ae964aa768775bec27a32d5339
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-10 22:00:01 +0300

    ReactNative: Updated Style; Colors

--- frontend/ReactNative/constants/styles.js
+++ frontend/ReactNative/constants/styles.js
@@ -1,6 +1,6 @@
 export const Colors = {
   primary500: "#4d79ff",
-  secondary500: "#1B3773",
+  secondary500: "#002B80",
   background500: "#e6eeff",
   error100: "#F34913",
   error200: "#F34913",
