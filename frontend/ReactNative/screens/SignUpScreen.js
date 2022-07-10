2b4927135bfca625729fbc7600c7adbe5e7f5dc6
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-11 01:37:13 +0300

    ReactNative: Added SignUpScreen Component

---
+++ frontend/ReactNative/screens/SignUpScreen.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+import Logo from "../ui/Logo";
+import BackIcon from "../ui/BackIcon";
+import FormSignUp from "../components/signup/FormSignUp";
+
+const SignUpScreen = () => {
+  const navigation = useNavigation();
+
+  function handleClick() {
+    navigation.replace("Login");
+  }
+  return (
+    <>
+      <View style={styles.mainContainer}>
+        <View style={styles.header}>
+          <BackIcon onPress={handleClick} />
+          <Logo logo="logo-min" />
+        </View>
+
+        <View style={styles.formContainer}>
+          <FormSignUp />
+        </View>
+      </View>
+    </>
+  );
+};
+
+export default SignUpScreen;
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  header: {
+    width: 330,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    alignSelf: "flex-start",
+    height: 200,
+    margin: 50,
+  },
+  formContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+  },
+});
