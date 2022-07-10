ae480dae9ba8833504fbd8e9e4c6665bd57b10ab
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-10 19:42:24 +0300

    ReactNative: Added: Button Component

---
+++ frontend/ReactNative/ui/Button.js
@@ -0,0 +1,45 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+import { Colors } from "../constants/styles";
+
+export function Button({ children, onPress }) {
+  return (
+    <Pressable
+      style={({ pressed }) => [styles.button, pressed && styles.pressed]}
+      onPress={onPress}
+    >
+      <View>
+        <Text style={styles.buttonText}>{children}</Text>
+      </View>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    width: 330,
+    height: 60,
+    borderRadius: 6,
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    // marginHorizontal: 10,
+    marginVertical: 10,
+    backgroundColor: Colors.primary500,
+    // backgroundColor: "#7862FF",
+    elevation: 2,
+    shadowColor: "black",
+    shadowOffset: { width: 1, height: 1 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+  },
+  pressed: {
+    opacity: 0.7,
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "white",
+    fontSize: 24,
+    fontWeight: "bold",
+    // fontFamily: "Montserrat",
+  },
+});
