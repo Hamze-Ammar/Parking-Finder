e10aefd3b252cc3a0b08a588fc53ca65b161acfb
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-10 21:59:22 +0300

    ReactNative: Added: SpecialButton Component

---
+++ frontend/ReactNative/ui/SpecialButton.js
@@ -0,0 +1,52 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+import { Colors } from "../constants/styles";
+
+export function SpecialButton({ children, onPress }) {
+  return (
+    <View style={styles.outerContainer}>
+      <Pressable
+        style={({ pressed }) => [styles.button, pressed && styles.pressed]}
+        onPress={onPress}
+      >
+        <View style={styles.textContainer}>
+          <Text style={styles.buttonText}>{children}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  outerContainer: {
+    marginTop: 40,
+  },
+  button: {
+    width: 330,
+    height: 60,
+    borderRadius: 6,
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    marginVertical: 10,
+    backgroundColor: Colors.secondary500,
+    elevation: 2,
+    shadowColor: "black",
+    shadowOffset: { width: 1, height: 1 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+  },
+  pressed: {
+    opacity: 0.7,
+  },
+  textContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "white",
+    fontSize: 16,
+    fontWeight: "100",
+  },
+});
