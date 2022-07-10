6ac36857a358de33e785d0154f94c75c0291930a
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-11 01:36:55 +0300

    ReactNative: Added LoginScreen Component

---
+++ frontend/ReactNative/screens/LoginScreen.js
@@ -0,0 +1,56 @@
+import { View, Text, Image, StyleSheet } from "react-native";
+
+import { Input } from "../ui/Input";
+import Form from "../components/login/Form";
+import Title from "../ui/Title";
+import Logo from "../ui/Logo";
+import CustomLink from "../components/login/CustomLink";
+
+const LoginScreen = () => {
+  return (
+    <>
+      <View style={styles.mainContainer}>
+        <View style={styles.logoContainer}>
+          <Logo logo="logo" />
+        </View>
+        {/* <Title>Sign In</Title> */}
+        <View style={styles.formContainer}>
+          <Form />
+        </View>
+        <View style={styles.footer}>
+          <CustomLink text="forget password" />
+          <CustomLink text="create an account" />
+        </View>
+      </View>
+    </>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+  },
+  logoContainer: {
+    flex: 2,
+    height: 200,
+    margin: 50,
+  },
+  formContainer: {
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+  },
+  footer: {
+    marginBottom: 20,
+    alignItems: "flex-end",
+    flex: 1,
+    width: 330,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+});
