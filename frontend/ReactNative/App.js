c644dff40c888f49916584f0f98fc20572f12fac
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-11 21:50:48 +0300

    ReactNative: Updated App.js

--- frontend/ReactNative/App.js
+++ frontend/ReactNative/App.js
@@ -1,41 +1,85 @@
+import { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import AppLoading from "expo-app-loading";
+import { Colors } from "./constants/styles";
 
 import LoginScreen from "./screens/LoginScreen";
 import SignUpScreen from "./screens/SignUpScreen";
-import { Colors } from "./constants/styles";
+import LandingScreen from "./screens/LandingScreen";
 
 const Stack = createNativeStackNavigator();
+
+function Root() {
+  const [isTryingLogin, setIsTryingLogin] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState();
+
+  useEffect(() => {
+    async function fetchToken() {
+      const storedToken = await AsyncStorage.getItem("token");
+      console.log({ storedToken });
+
+      if (storedToken) {
+        setIsAuthenticated(true);
+      }
+
+      setIsTryingLogin(false);
+    }
+
+    fetchToken();
+  }, []);
+
+  if (isTryingLogin) {
+    return <AppLoading />;
+  }
+  return <Navigation isAuthenticated={isAuthenticated} />;
+}
+
+function Navigation({ isAuthenticated }) {
+  return (
+    <NavigationContainer>
+      {!isAuthenticated && <AuthStack />}
+      {isAuthenticated && <AuthenticatedStack />}
+    </NavigationContainer>
+  );
+}
+
+function AuthStack() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Signup" component={SignUpScreen} />
+    </Stack.Navigator>
+  );
+}
+
+function AuthenticatedStack() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: Colors.primary500 },
+        headerTintColor: "white",
+        contentStyle: { backgroundColor: Colors.primary100 },
+      }}
+    >
+      <Stack.Screen name="landing" component={LandingScreen} />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <>
-      <StatusBar style="auto" />
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerStyle: { backgroundColor: Colors.background500 },
-            headerTintColor: Colors.primary500,
-            contentStyle: { backgroundColor: "white" },
-            headerShown: false,
-          }}
-        >
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Signup" component={SignUpScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <StatusBar style="light" />
+      <Root />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
