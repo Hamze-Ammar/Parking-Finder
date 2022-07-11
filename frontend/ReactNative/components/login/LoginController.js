720523794f3df6d6d05f84a0e6df17c27fa77347
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-11 20:30:50 +0300

    ReactNative: Added HandleResponse LoginController

--- frontend/ReactNative/components/login/LoginController.js
+++ frontend/ReactNative/components/login/LoginController.js
@@ -1,15 +1,38 @@
 import { Alert } from "react-native";
 
-const LoginUser = () => {
-  return;
+const LoginUser = async (credentials) => {
+  try {
+    const res = await fetch("http://192.168.1.95:8000/api/v1/user/login", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(credentials),
+    });
+    const data = await res.json();
+    // console.log(data);
+    handleResponse(data);
+  } catch (err) {
+    console.log(err);
+  }
 };
-
 export default LoginUser;
+
+const handleResponse = (data) => {
+  //   console.log(data.error);
+  if (data.error) {
+    if (data.error === "Unauthorized") {
+      Alert.alert("Invalid credentials", "Wrong email or password!");
+    }
+  }
+  if (data.access_token) {
+    Alert.alert("Success", "You are now logged in");
+  }
+};
 
 export function validateInput(credentials) {
   let { email, password } = credentials;
 
-  console.log({ email });
   email = email.trim();
   password = password.trim();
 
