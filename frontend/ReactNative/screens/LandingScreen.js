492a1afd1dc50f68cc928f2afe88177f79e5f2ed
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-11 21:49:23 +0300

    ReactNative: Added LandingScreen

---
+++ frontend/ReactNative/screens/LandingScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const LandingScreen = () => {
+  return (
+    <View>
+      <Text>LandingScreen</Text>
+    </View>
+  );
+};
+
+export default LandingScreen;
