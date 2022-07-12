bf9a7e80265f59e5eecc4722743ca517d45d0920
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-13 02:00:55 +0300

    ReactNative: Added ParkingScreen Component

---
+++ frontend/ReactNative/screens/authenticatedScreens/ParkingScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ParkingScreen = () => {
+  return (
+    <View>
+      <Text>ParkingScreen</Text>
+    </View>
+  );
+};
+
+export default ParkingScreen;
+
+const styles = StyleSheet.create({});
