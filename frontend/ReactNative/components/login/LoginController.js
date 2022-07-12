2864c20092ccb3b3e0dc0421abc3a1b2cc44c2e4
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-12 22:41:21 +0300

    ReactNative: Fixed LoginController Component

--- frontend/ReactNative/components/login/LoginController.js
+++ frontend/ReactNative/components/login/LoginController.js
@@ -1,8 +1,26 @@
 import { Alert } from "react-native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useNavigation } from "@react-navigation/native";
 
-const LoginUser = async (credentials) => {
+export const loginUser = async (credentials) => {
+
+  const handleResponse = (data) => {
+    if (data["email"]) {
+      if (data["email"][0]) {
+        Alert.alert(data["email"][0]);
+        return null;
+      }
+    }
+    if (data.error) {
+      if (data.error === "Unauthorized") {
+        Alert.alert("Invalid credentials", "Wrong email or password!");
+        return null;
+      }
+    }
+    if (data.access_token) {
+      let token = data.access_token;
+      return token;
+    }
+  };
+
   try {
     const res = await fetch("http://192.168.1.95:8000/api/v1/user/login", {
       method: "POST",
@@ -12,23 +30,9 @@ const LoginUser = async (credentials) => {
       body: JSON.stringify(credentials),
     });
     const data = await res.json();
-    handleResponse(data);
+    return handleResponse(data);
   } catch (err) {
     console.log(err);
-  }
-};
-export default LoginUser;
-
-const handleResponse = (data) => {
-  if (data.error) {
-    if (data.error === "Unauthorized") {
-      Alert.alert("Invalid credentials", "Wrong email or password!");
-    }
-  }
-  if (data.access_token) {
-    AsyncStorage.setItem("token", data.access_token);
-    const navigation = useNavigation();
-    navigation.replace("landing");
   }
 };
 
@@ -50,3 +54,4 @@ export function validateInput(credentials) {
   }
   return "valid";
 }
+
