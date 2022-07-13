45693368fd3d9f1f075fcf1522ee69b5a3a02ef8
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-13 12:40:32 +0300

    ReactNative: Applied Persmission and getLocation

--- frontend/ReactNative/components/landingPage/LandingPage.js
+++ frontend/ReactNative/components/landingPage/LandingPage.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Alert } from "react-native";
+import {
+  getCurrentPositionAsync,
+  PermissionStatus,
+  useForegroundPermissions,
+  reverseGeocodeAsync,
+} from "expo-location";
+
 import Title from "../../ui/Title";
 import { Button } from "../../ui/Button";
 
@@ -7,9 +14,48 @@ import { useNavigation } from "@react-navigation/native";
 
 const LandingPage = () => {
   const navigation = useNavigation();
+  const [locationPermissionInformation, requestPermission] =
+    useForegroundPermissions();
+
+  async function verifyPermissions() {
+    if (
+      locationPermissionInformation.status === PermissionStatus.UNDETERMINED
+    ) {
+      const permissionResponse = await requestPermission();
+
+      return permissionResponse.granted;
+    }
+    if (locationPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient Permissions!",
+        "You need to grant location permissions to use this app"
+      );
+      return false;
+    }
+    return true;
+  }
+
+  async function getLocationHandler() {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+    const location = await getCurrentPositionAsync();
+    console.log(location);
+    let latitude = location.coords.latitude;
+    let longitude = location.coords.longitude;
+    // console.log({ latitude }, { latitude });
+    let regionName = await reverseGeocodeAsync({
+      latitude: latitude,
+      longitude: longitude,
+    });
+    console.log(regionName);
+  }
 
   function handleClick() {
-    navigation.replace("parking");
+    getLocationHandler();
+    // navigation.replace("parking");
   }
 
   return (
