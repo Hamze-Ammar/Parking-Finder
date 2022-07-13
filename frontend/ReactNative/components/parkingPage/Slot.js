d503af6a7e527d022f5cf7193a3a817434356474
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-13 20:16:11 +0300

    ReactNative: Fixed Slot.js Component

--- frontend/ReactNative/components/parkingPage/Slot.js
+++ frontend/ReactNative/components/parkingPage/Slot.js
@@ -1,20 +1,61 @@
 import { StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
+import { Colors } from "../../constants/styles";
 
-const Slot = ({ name }) => {
+const Slot = ({ name, side, number }) => {
   var test = {
     empty: require("../../assets/images/emptySlot.png"),
     left: require("../../assets/images/busySlotLeft.png"),
     right: require("../../assets/images/busySlotRight.png"),
   };
 
+  let color = false;
+  if (name === "empty") {
+    color = true;
+  }
+
+  if (side === "right") {
+    return (
+      <View style={[styles.slotContainer, styles.slotContainerReverse]}>
+        <Image source={test[name]} />
+        <Text
+          style={[styles.text, styles.textReverse, color && styles.colored]}
+        >
+          {number}
+        </Text>
+      </View>
+    );
+  }
+
   return (
-    <View>
+    <View style={styles.slotContainer}>
       <Image source={test[name]} />
+      <Text style={[styles.text, , color && styles.colored]}>{number}</Text>
     </View>
   );
 };
 
 export default Slot;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  slotContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  text: {
+    marginHorizontal: 8,
+    color: Colors.secondary500,
+    fontSize: 28,
+    transform: [{ rotate: "90deg" }],
+    fontWeight: "bold",
+  },
+  slotContainerReverse: {
+    flexDirection: "row-reverse",
+  },
+  textReverse: {
+    transform: [{ rotate: "-90deg" }],
+  },
+  colored: {
+    color: Colors.primary500,
+  },
+});
