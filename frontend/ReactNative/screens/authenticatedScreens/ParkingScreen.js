2638724857ad9b17fb6cdc8a32e049700e8c2976
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-15 00:12:01 +0300

    RN: Fixed a bug ParkingScreen.js

--- frontend/ReactNative/screens/authenticatedScreens/ParkingScreen.js
+++ frontend/ReactNative/screens/authenticatedScreens/ParkingScreen.js
@@ -1,8 +1,9 @@
 import ParkingView from "../../components/parkingPage/ParkingView";
 
 const ParkingScreen = ({ route }) => {
-  if (route) {
+  if (route?.params) {
     const param = route.params;
+    // const city_name = param.city[0].subregion;
     const city_name = param.city[0].subregion;
     return <ParkingView city_name={city_name} />;
   }
