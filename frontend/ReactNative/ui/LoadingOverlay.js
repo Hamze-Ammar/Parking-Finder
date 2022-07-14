f6495737942534cf12b152843e8d04e482653926
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-15 00:10:39 +0300

    RN: Added Color LoadingOverlay

--- frontend/ReactNative/ui/LoadingOverlay.js
+++ frontend/ReactNative/ui/LoadingOverlay.js
@@ -1,10 +1,11 @@
 import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import { Colors } from "../constants/styles";
 
 function LoadingOverlay({ message }) {
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.message}>{message}</Text>
-      <ActivityIndicator size="large" />
+      <ActivityIndicator size="large" color={Colors.primary500} />
     </View>
   );
 }
