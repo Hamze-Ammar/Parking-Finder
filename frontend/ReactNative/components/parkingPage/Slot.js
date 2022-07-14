2eef7310457694a4b951cd071b40c7d09125fc08
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-14 16:19:51 +0300

    RN: Updated Slot.js Add Reservation Condition

--- frontend/ReactNative/components/parkingPage/Slot.js
+++ frontend/ReactNative/components/parkingPage/Slot.js
@@ -1,37 +1,67 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, Pressable } from "react-native";
 import React from "react";
 import { Colors } from "../../constants/styles";
+import { sendReservation } from "./parkingController";
+import ReservedSlot from "./ReservedSlot";
 
-const Slot = ({ name, side, number }) => {
-  var test = {
+const Slot = ({ id, name, side, number, SetRefresh, is_reserved }) => {
+  const test = {
     empty: require("../../assets/images/emptySlot.png"),
     left: require("../../assets/images/busySlotLeft.png"),
     right: require("../../assets/images/busySlotRight.png"),
   };
+  // console.log(is_reserved == true);
+
+  function onPress() {
+    if (name === "empty") {
+      sendReservation(id);
+      SetRefresh();
+    }
+  }
 
   let color = false;
   if (name === "empty") {
     color = true;
   }
 
+  // if (is_reserved == true) {
+  //   return <>
+  //   <ReservedSlot />
+  //   </>
+  // }
+
   if (side === "right") {
     return (
-      <View style={[styles.slotContainer, styles.slotContainerReverse]}>
-        <Image source={test[name]} />
-        <Text
-          style={[styles.text, styles.textReverse, color && styles.colored]}
-        >
-          {number}
-        </Text>
-      </View>
+      <Pressable
+        style={({ pressed }) => [styles.button, pressed && styles.pressed]}
+        onPress={onPress}
+      >
+        <View style={[styles.slotContainer, styles.slotContainerReverse]}>
+          {is_reserved ? (
+            <ReservedSlot />
+          ) : (
+            <Image style={styles.image} source={test[name]} />
+          )}
+          <Text
+            style={[styles.text, styles.textReverse, color && styles.colored]}
+          >
+            {number}
+          </Text>
+        </View>
+      </Pressable>
     );
   }
 
   return (
-    <View style={styles.slotContainer}>
-      <Image source={test[name]} />
-      <Text style={[styles.text, , color && styles.colored]}>{number}</Text>
-    </View>
+    <Pressable
+      style={({ pressed }) => [styles.button, pressed && styles.pressed]}
+      onPress={onPress}
+    >
+      <View style={styles.slotContainer}>
+        <Image source={test[name]} />
+        <Text style={[styles.text, , color && styles.colored]}>{number}</Text>
+      </View>
+    </Pressable>
   );
 };
 
@@ -57,5 +87,11 @@ const styles = StyleSheet.create({
   },
   colored: {
     color: Colors.primary500,
+  },
+  pressed: {
+    opacity: 0.7,
+  },
+  image: {
+    opacity: 0.5,
   },
 });
