a640661b6e6f7c31cce52d7de3045106efaea63d
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-14 16:18:19 +0300

    RN: Added sendReservation func to ParkingView.js

--- frontend/ReactNative/components/parkingPage/parkingController.js
+++ frontend/ReactNative/components/parkingPage/parkingController.js
@@ -12,7 +12,7 @@ export const getParkingById = async (id) => {
   }
 
   const res = await fetch(
-    "http://192.168.1.95:8000/api/v1/user/viewParking/1",
+    `http://192.168.1.95:8000/api/v1/user/viewParking/${id}`,
     {
       method: "GET",
       headers: {
@@ -25,8 +25,34 @@ export const getParkingById = async (id) => {
   if (data.status === "Success") {
     return data;
   } else {
-    return null;
+    return false;
   }
 };
 
-// getParkingById("1");
+// Send Request Reservation
+export const sendReservation = async (id) => {
+  console.log("reservation sent on slot:  ", id);
+  let token = await AsyncStorage.getItem("token");
+  if (!token) {
+    return;
+  }
+  const res = await fetch(
+    `http://192.168.1.95:8000/api/v1/user/makeReservation/${id}`,
+    {
+      method: "PUT",
+      headers: {
+        Authorization: "Bearer " + token,
+        "Content-type": "application/json",
+      },
+    }
+  );
+  const data = await res.json();
+  if (data.status === "Success") {
+    // console.log(data);
+    return data;
+  } else {
+    return false;
+  }
+};
+
+// sendReservation("67");
