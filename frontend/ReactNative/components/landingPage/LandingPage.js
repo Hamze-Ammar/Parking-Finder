eee092f0168371532291ee1baa24dc1436d28409
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-15 21:16:13 +0300

    RN: Updated LandingPage.js

--- frontend/ReactNative/components/landingPage/LandingPage.js
+++ frontend/ReactNative/components/landingPage/LandingPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, Alert } from "react-native";
+import { View, Text, StyleSheet, Alert, Image } from "react-native";
 import {
   getCurrentPositionAsync,
   PermissionStatus,
@@ -64,21 +64,40 @@ const LandingPage = () => {
       return permissionResponse.granted;
     }
     const regionName = await getLocationHandler();
+    // Should navigate to map screen
     navigation.navigate("parking", {
       city: regionName,
     });
     setIsLoading(false);
   }
 
+  // Preparing images
+  // const images = {
+  //   sportCar: require("../../assets/images/Sportscar.gif"),
+  // };
+
   if (isLoading) {
     return <LoadingOverlay />;
   }
 
   return (
-    <View style={styles.textContainer}>
-      <Title myFontSize={35}>Reinventing The Wheels</Title>
-      <Title myFontSize={22}>Life is too short to be spent on PARKING!</Title>
-      <Button onPress={handleClick}>GO</Button>
+    <View style={styles.mainContainer}>
+      <Title myFontSize={48}>Reinventing The Wheels</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          style={{ width: 340, height: 170 }}
+          source={{
+            uri: "https://cdn.dribbble.com/users/441371/screenshots/2231734/media/b690d5542e4929819b713593361bd010.gif",
+          }}
+        />
+      </View>
+
+      <View style={styles.btnContainer}>
+        <Title style={styles.subtitleContainer} myFontSize={18}>
+          Life is too short to be spent on PARKING!
+        </Title>
+        <Button onPress={handleClick}>GO</Button>
+      </View>
     </View>
   );
 };
@@ -86,12 +105,26 @@ const LandingPage = () => {
 export default LandingPage;
 
 const styles = StyleSheet.create({
-  textContainer: {
+  mainContainer: {
     flex: 1,
     alignItems: "center",
     textAlign: "center",
     alignSelf: "center",
     margin: 15,
     marginBottom: 25,
+  },
+  btnContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+  },
+  subtitleContainer: {
+    fontSize: 20,
+  },
+  imageContainer: {
+    borderRadius: 8,
+    overflow: "hidden",
+    opacity: 0.8,
   },
 });
