a2747c2f9f51c67e3da1ce21a02bc5e07f5263d0
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-15 21:14:56 +0300

    RN: Added UserProfile Screen

---
+++ frontend/ReactNative/screens/DrawerScreens/UserProfile.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const UserProfile = () => {
+  return (
+    <View>
+      <Text>Profile</Text>
+    </View>
+  );
+};
+
+export default UserProfile;
+
+const styles = StyleSheet.create({});
