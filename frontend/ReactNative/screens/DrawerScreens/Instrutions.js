002630fb9dcda4f80f24348977593896b61dd1df
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-15 21:15:18 +0300

    RN: Added Instruction.js

---
+++ frontend/ReactNative/screens/DrawerScreens/Instrutions.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Instrutions = () => {
+  return (
+    <View>
+      <Text>Instrutions</Text>
+    </View>
+  );
+};
+
+export default Instrutions;
+
+const styles = StyleSheet.create({});
