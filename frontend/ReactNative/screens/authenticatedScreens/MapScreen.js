33dcb386c6f0d923312f0854f9e366cee648dc76
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-21 01:50:38 +0300

    RN | MapScreen Added PopupInfo

--- frontend/ReactNative/screens/authenticatedScreens/MapScreen.js
+++ frontend/ReactNative/screens/authenticatedScreens/MapScreen.js
@@ -13,15 +13,22 @@ import { getParkingByCityName } from "../../components/mapFolder/mapController";
 import { FavoritesContext } from "../../store/favorites-context";
 import { AuthContext } from "../../store/auth-context";
 import { mapStyleLight, mapStyleDark } from "../../constants/mapStyle";
+import PopupParking from "../../components/mapFolder/PopupParking";
+import { dimensions } from "../../constants/styles";
+import { displayPopup } from "../../components/mapFolder/mapController";
+
 const MapScreen = ({ route, navigation }) => {
   const favoritesCtx = useContext(FavoritesContext);
   const authCtx = useContext(AuthContext);
   const [token, setToken] = useState(authCtx.token || null);
   const [city, setCity] = useState(route.params?.city || null);
-  const [parkings, setParkings] = useState();
+  const [latitude, setLatitude] = useState(route.params?.latitude || null);
+  const [longitude, setLongitude] = useState(route.params?.longitude || null);
+  const [parkings, setParkings] = useState([]);
   const [isDark, setIsDark] = useState(false);
-  //   console.log(city, token);
-  //   console.log({ parkings });
+  const [parkingPop, setParkingPop] = useState();
+  //   console.log(city, token, latitude, longitude);
+  //   console.log({ parkingPop });
   useEffect(() => {
     async function reloadData() {
       if (city && token) {
@@ -33,35 +40,47 @@ const MapScreen = ({ route, navigation }) => {
     reloadData();
   }, [city, token]);
 
+  //=============================
   return (
     <>
       <MapView
         style={styles.container}
         customMapStyle={isDark ? mapStyleDark : mapStyleLight}
         initialRegion={{
-          latitude: parseFloat(33.8911743),
-          longitude: parseFloat(35.5059504),
+          latitude: parseFloat(latitude),
+          longitude: parseFloat(longitude),
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
+        onPress={() => setParkingPop(null)}
       >
         {parkings &&
           parkings.map((parking) => {
+            // console.log(parking);
             let location = JSON.parse(parking.location);
-            let latitude = location.latitude;
-            let longitude = location.longitude;
+            let latitudeDes = location.latitude;
+            let longitudeDes = location.longitude;
             let description = parking.freeSlots + " Available Slots";
             let id = parking.id;
             return (
               <Marker
                 coordinate={{
-                  latitude: parseFloat(latitude),
-                  longitude: parseFloat(longitude),
+                  latitude: parseFloat(latitudeDes),
+                  longitude: parseFloat(longitudeDes),
                 }}
                 icon={require("../../assets/images/iconMarker2.png")}
                 key={id}
                 title={parking.name}
                 description={description}
+                onPress={async () => {
+                  setParkingPop(
+                    await displayPopup({
+                      parking: parking,
+                      latitude: latitude,
+                      longitude: longitude,
+                    })
+                  );
+                }}
                 onCalloutPress={() => {
                   navigation.navigate("Parking", {
                     city: { city },
@@ -79,12 +98,7 @@ const MapScreen = ({ route, navigation }) => {
           icon={require("../../assets/images/iconHere3.png")}
           title="PIN"
           description="You are here!"
-          onCalloutPress={() => {
-            getParkingByCityName({
-              city: city,
-              token: token,
-            });
-          }}
+          //   onCalloutPress={calculateDistance}
         />
 
         {/* <Marker
@@ -95,6 +109,9 @@ const MapScreen = ({ route, navigation }) => {
           }}
         /> */}
       </MapView>
+      <View style={styles.popupInfo}>
+        {parkingPop && <PopupParking parking={parkingPop} city={city} />}
+      </View>
       <View style={styles.themIcon}>
         <MaterialCommunityIcons
           name="theme-light-dark"
@@ -120,6 +137,12 @@ const styles = StyleSheet.create({
     top: 50,
     right: 40,
     color: "gray",
+  },
+  popupInfo: {
+    position: "absolute",
+    bottom: 60,
+    left: Math.round(dimensions.width / 20),
+    right: Math.round(dimensions.width / 20),
   },
 });
 
