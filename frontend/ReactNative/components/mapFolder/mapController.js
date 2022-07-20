1804c18fc8fabd25f8061383ca1de362183a2100
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-21 01:52:35 +0300

    RN | Added displayPopup in mapController

--- frontend/ReactNative/components/mapFolder/mapController.js
+++ frontend/ReactNative/components/mapFolder/mapController.js
@@ -4,7 +4,7 @@ export const getParkingByCityName = async ({ city, token }) => {
   if (!city || !token) {
     return;
   }
-  //   console.log("wer here");
+  console.log("wer here");
   const res = await fetch(`${URL}/info/getParkingsByCityName/${city}`, {
     method: "GET",
     headers: {
@@ -13,7 +13,7 @@ export const getParkingByCityName = async ({ city, token }) => {
     },
   });
   const data = await res.json();
-  //   console.log(data);
+  console.log(data);
   if (data.status === "Success") {
     let response = await handleResponse(data);
     return response;
@@ -26,13 +26,14 @@ function handleResponse(data) {
   if (!!data.res) {
     let parkings = [];
     data.res.map((item) => {
-      //   console.log("idparkingcheck: ", item.id);
+      //   console.log("idparkingcheck: ", item.opening_hr);
       let parking = {
         id: item.id,
         name: item.name,
         freeSlots: item.freeSlots,
         location: item.location,
-        // location: item.location,
+        opening_hr: item.opening_hr,
+        closing_hr: item.closing_hr,
       };
       parkings.push(parking);
     });
@@ -43,3 +44,61 @@ function handleResponse(data) {
     return parkings;
   }
 }
+
+export const displayPopup = async ({ parking, latitude, longitude }) => {
+  if (!parking) {
+    return;
+  }
+  let location = JSON.parse(parking.location);
+  let latitudeDes = location.latitude;
+  let longitudeDes = location.longitude;
+  //   console.log(parking);
+  //   console.log(parking.id);
+  let response = await calculateDistance(
+    latitude,
+    longitude,
+    latitudeDes,
+    longitudeDes
+  );
+  if (response) {
+    let distance = response.distances[0][0];
+    let duration = response.durations[0][0];
+    let obj = {
+      id: parking.id,
+      name: parking.name,
+      opening_hr: parking.opening_hr,
+      closing_hr: parking.closing_hr,
+      distance: distance,
+      duration: duration,
+    };
+    return obj;
+  }
+};
+
+//========================================
+//https://rapidapi.com/trueway/api/trueway-matrix/details
+const calculateDistance = (
+  latitudeOrg,
+  longitudeOrg,
+  latitudeDes,
+  longitudeDes
+) => {
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "7dd0902cf9msha9e2a44c12f5bf9p1df25ejsn2054907ccc4a",
+      "X-RapidAPI-Host": "trueway-matrix.p.rapidapi.com",
+    },
+  };
+  let origin = `${latitudeOrg},${longitudeOrg}`;
+  let destination = `${latitudeDes},${longitudeDes}`;
+  return fetch(
+    `https://trueway-matrix.p.rapidapi.com/CalculateDrivingMatrix?origins=${origin}&destinations=${destination}`,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      return response;
+    })
+    .catch((err) => console.error(err));
+};
