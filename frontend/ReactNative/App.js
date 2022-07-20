f9b6894a806bf18abc727aa26c72b8d1c1c5b384
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-21 01:53:02 +0300

    RN | App.js Added New StackNavigator

--- frontend/ReactNative/App.js
+++ frontend/ReactNative/App.js
@@ -8,7 +8,6 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { FontAwesome } from "@expo/vector-icons";
-import { FontAwesome5 } from "@expo/vector-icons";
 import AuthContextProvider, { AuthContext } from "./store/auth-context";
 import FavoritesContextProvider, {
   FavoritesContext,
@@ -24,6 +23,7 @@ import ParkingScreen from "./screens/authenticatedScreens/ParkingScreen";
 import Favourites from "./screens/TabScreens/Favourites";
 import ProfileScreen from "./screens/TabScreens/editProfile/ProfileScreen";
 import MapScreen from "./screens/authenticatedScreens/MapScreen";
+// import HomeStackNavigator from "./stacks/HomeStackNavigator";
 
 // Handling error Require cycle
 LogBox.ignoreLogs(["Require cycle:"]);
@@ -32,6 +32,33 @@ global.__reanimatedWorkletInit = () => {};
 const Stack = createNativeStackNavigator();
 const BottomTab = createBottomTabNavigator();
 
+function HomeStackNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Parking Finder"
+        component={LandingScreen}
+        options={{
+          headerStyle: {
+            backgroundColor: Colors.primary500,
+          },
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+          headerTintColor: "white",
+          tabBarHideOnKeyboard: true,
+          tabBarActiveTintColor: Colors.primary500,
+          tabBarInactiveTintColor: "gray",
+        }}
+      />
+      <Stack.Screen
+        name="map"
+        component={MapScreen}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+}
 function BottomTabNavigator() {
   const authCtx = useContext(AuthContext);
   return (
@@ -46,12 +73,13 @@ function BottomTabNavigator() {
       }}
     >
       <BottomTab.Screen
-        name="Parking Finder"
-        component={LandingScreen}
+        name="Home"
+        component={HomeStackNavigator}
         options={{
           tabBarIcon: ({ color, size }) => (
             <Ionicons name="home" color={color} size={size} />
           ),
+          headerShown: false,
         }}
       />
       <BottomTab.Screen
@@ -63,16 +91,16 @@ function BottomTabNavigator() {
           ),
         }}
       />
-      <BottomTab.Screen
+      {/* <BottomTab.Screen
         name="map"
         component={MapScreen}
         options={{
           tabBarIcon: ({ color, size }) => (
             <Ionicons name="map-outline" color={color} size={size} />
           ),
-          headerShown: false,
+          headerShown: false
         }}
-      />
+      /> */}
       <BottomTab.Screen
         name="Favourite Parkings"
         component={Favourites}
