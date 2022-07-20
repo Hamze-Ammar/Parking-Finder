e2088f47d45b8a3c19e516f5a3f668fabc2e5cd3
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-20 03:00:16 +0300

    RN | Added mapController

--- frontend/ReactNative/components/mapFolder/mapController.js
+++ frontend/ReactNative/components/mapFolder/mapController.js
@@ -1,3 +1,45 @@
-export const getParkingByCityName = (city, token) => {
-  console.log(city, token);
+import { URL } from "../../constants/backendSync";
+
+export const getParkingByCityName = async ({ city, token }) => {
+  if (!city || !token) {
+    return;
+  }
+  //   console.log("wer here");
+  const res = await fetch(`${URL}/info/getParkingsByCityName/${city}`, {
+    method: "GET",
+    headers: {
+      Authorization: "Bearer " + token,
+      "Content-type": "application/json",
+    },
+  });
+  const data = await res.json();
+  //   console.log(data);
+  if (data.status === "Success") {
+    let response = await handleResponse(data);
+    return response;
+  } else {
+    return false;
+  }
 };
+
+function handleResponse(data) {
+  if (!!data.res) {
+    let parkings = [];
+    data.res.map((item) => {
+      //   console.log("idparkingcheck: ", item.id);
+      let parking = {
+        id: item.id,
+        name: item.name,
+        freeSlots: item.freeSlots,
+        location: item.location,
+        // location: item.location,
+      };
+      parkings.push(parking);
+    });
+    // console.log(parkings[0].location["latitude"]);
+    // let test = JSON.parse(parkings[0].location);
+    // console.log(test.latitude);
+    // console.log({ parkings });
+    return parkings;
+  }
+}
