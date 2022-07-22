7ee08e77020b68efcccef0d9ff9b426a0fd9487e
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-22 17:59:29 +0300

    Laravel | Updated changeSlotState

--- backend-Laravel/app/Http/Controllers/General/SensorController.php
+++ backend-Laravel/app/Http/Controllers/General/SensorController.php
@@ -11,8 +11,15 @@ class SensorController extends Controller
     // mainly from the sensors
     public function changeSlotState($id)
     {
+
         $slot = Slot::find($id);
-        $slot->is_available = '0';
+
+        $curr_state = $slot->is_available;
+        if ($curr_state == 0){
+            $slot->is_available = 1;
+        }else{
+            $slot->is_available = 0;
+        }
         $slot->save();
 
         return response()->json([
