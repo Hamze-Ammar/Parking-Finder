2aaf8974b99fd330f8cb89ff0dbcaafaf2dcc52d
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-22 15:11:06 +0300

    Laravel | Added LocationController

---
+++ backend-Laravel/app/Http/Controllers/LocationController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Parking;
+
+
+class LocationController extends Controller
+{
+        // ---------------- [ Load View ] ----------------
+        public function index(Request $request) {
+    
+            $lat = 33.8911736;
+            $lon = 35.5059611;
+            // die($lat);
+        
+            // $data = DB::table("parkings")
+            //     ->select("parkings.id"
+            //         ,DB::raw("6371 * acos(cos(radians(" . $lat . ")) 
+            //         * cos(radians(parkings.lat)) 
+            //         * cos(radians(parkings.lon) - radians(" . $lon . ")) 
+            //         + sin(radians(" .$lat. ")) 
+            //         * sin(radians(parkings.lat))) AS distance"))
+            //         ->groupBy("parkings.id")
+            //         ->get();
+
+
+            // dd($data);
+    }
+
+    public function findNearestParkings(Request $request)
+    {
+        $latitude = $request->lat;
+        $longitude = $request->long;
+        $radius = 400;
+        // die($latitude);
+        /*
+         * using eloquent approach, make sure to replace the "Restaurant" with your actual model name
+         * replace 6371000 with 6371 for kilometer and 3956 for miles
+         */
+        $parkings = Parking::selectRaw("id, name, latitude, longitude,
+                         ( 6371000 * acos( cos( radians(?) ) *
+                           cos( radians( latitude ) )
+                           * cos( radians( longitude ) - radians(?)
+                           ) + sin( radians(?) ) *
+                           sin( radians( latitude ) ) )
+                         ) AS distance", [$latitude, $longitude, $latitude])
+            ->where('is_approved', '=', 1)
+            ->having("distance", "<", $radius)
+            ->orderBy("distance",'asc')
+            ->offset(0)
+            ->limit(20)
+            ->get();
+
+        return $parkings;
+    }
+}
