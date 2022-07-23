964736b93973c42f92c38adab1290d1a62422c1c
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-23 17:36:11 +0300

    RJS | Added CONSTANT color.js

---
+++ frontend/reactjs/src/constant/color.js
@@ -0,0 +1,11 @@
+export const Colors = {
+  primary500: "#7862FF",
+  secondary500: "#1B3773",
+  background500: "#e6eeff",
+  background200: "#FFFCFC",
+  error100: "#F34913",
+  error200: "#F34913",
+  error500: "#F34913",
+  marked400: "#ff78f1",
+  earning500: "#f1c232",
+};
