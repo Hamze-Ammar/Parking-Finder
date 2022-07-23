3e3785f04d803f69f5e96d69d5611fc4f75dbf1d
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-23 17:36:33 +0300

    RJS | Added Packages & Fonts

--- frontend/reactjs/src/App.js
+++ frontend/reactjs/src/App.js
@@ -1,5 +1,9 @@
+import LandingPage from "./pages/landingPage/LandingPage";
+
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <LandingPage/>
+  </div>;
 }
 
 export default App;
--- frontend/reactjs/src/index.js
+++ frontend/reactjs/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css";
+import './App.css'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
