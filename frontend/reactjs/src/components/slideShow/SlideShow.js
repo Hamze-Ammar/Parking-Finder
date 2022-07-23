cd2ce182490ca77dda20baa2e5af228b8647ce5c
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-23 22:50:34 +0300

    RJS | Added SlideShow.js & styles.css

---
+++ frontend/reactjs/src/components/slideShow/SlideShow.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Slide } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
+import "./styles.css";
+import pic from "../../assets/images/App1.jpeg";
+import pic2 from "../../assets/images/App2.jpeg";
+import pic3 from "../../assets/images/App3.jpeg";
+
+const slideImages = [
+  {
+    src: pic,
+    // caption: "Slide 1",
+  },
+  {
+    src: pic2,
+    // caption: "Slide 2",
+  },
+  {
+    src: pic3,
+    // caption: "Slide 3",
+  },
+];
+
+const Slideshow = () => {
+  return (
+    <div className="slide-container">
+      <Slide>
+        {slideImages.map((slideImage, index) => {
+          console.log(slideImage.src);
+          return (
+            <div className="each-slide" key={index}>
+              <div
+                className="each-slide-img"
+                style={{ backgroundImage: `url(${slideImage.src})` }}
+              >
+                {/* <span className="each-slide-caption">{slideImage.caption}</span> */}
+              </div>
+            </div>
+          );
+        })}
+      </Slide>
+    </div>
+  );
+};
+
+export default Slideshow;
