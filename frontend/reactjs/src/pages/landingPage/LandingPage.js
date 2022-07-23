3040152c08c72dd54582d297ae1811ed8d5f71c8
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-23 22:51:22 +0300

    RJS | Added LandingPage.js

--- frontend/reactjs/src/pages/landingPage/LandingPage.js
+++ frontend/reactjs/src/pages/landingPage/LandingPage.js
@@ -7,6 +7,8 @@ import {
   FaTwitter,
   FaEnvelope,
 } from "react-icons/fa";
+import Logo from "../../ui/Logo";
+import SlideShow from "../../components/slideShow/SlideShow";
 
 const LandingPage = () => {
   const classes = useStyles();
@@ -14,18 +16,57 @@ const LandingPage = () => {
     <>
       <div className={classes.layout}>
         <div className={classes.container}>
-          <div>header</div>
-          <div>center</div>
+          <div className={classes.header}></div>
+          <div className={classes.body}>
+            <div className={classes.bodyLeft}>
+              <div>
+                <Logo />
+              </div>
+              <div>
+                Join 802 <br /> Parkings <br />
+                around <br /> the world
+              </div>
+              <div className={classes.bodyLeftLink}>
+                become a partner? <span className={classes.register}>Register Now</span> 
+              </div>
+            </div>
+            <div className={classes.slideShowContainer}>
+              <SlideShow />
+            </div>
+            <div className={classes.rightContainer}>
+              <div className={classes.login}>Login</div>
+              <div>
+                <div className={classes.bold}>
+                  Life is too short <br /> to be spent on <br /> PARKING! <br />
+                </div>
+                <div className={classes.rightBottom}>
+                  <div style={{ fontSize: "24px" }}>
+                    Download <br /> Our App <br /> For Free
+                  </div>
+                  <div className={classes.logosContainer}>
+                    <div>
+                      <img
+                        src={require("../../assets/images/GooglePlay.png")}
+                      />
+                    </div>
+                    <div>
+                      <img src={require("../../assets/images/AppStore.png")} />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <div className={classes.footer}>
           <div></div>
           <div>Terms and Conditions</div>
           <div className={classes.footerIconContainer}>
-            <FaFacebook className={classes.footerIcon}/>
-            <FaInstagram className={classes.footerIcon}/>
-            <FaFacebookMessenger className={classes.footerIcon}/>
-            <FaTwitter className={classes.footerIcon}/>
-            <FaEnvelope className={classes.footerIcon}/>
+            <FaFacebook className={classes.footerIcon} />
+            <FaInstagram className={classes.footerIcon} />
+            <FaFacebookMessenger className={classes.footerIcon} />
+            <FaTwitter className={classes.footerIcon} />
+            <FaEnvelope className={classes.footerIcon} />
           </div>
         </div>
       </div>
