146b8029d38ba2399f7bc1639c5f55fa53baee4b
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-23 13:40:27 +0300

    RN | CircularUserRequests.js Added Functionality

--- frontend/ReactNative/screens/TabScreens/editProfile/CircularUserRequests.js
+++ frontend/ReactNative/screens/TabScreens/editProfile/CircularUserRequests.js
@@ -1,26 +1,58 @@
 import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import CircularProgress from "react-native-circular-progress-indicator";
 import { Colors } from "../../../constants/styles";
 import { Ionicons } from "@expo/vector-icons";
+import { getOverviewInfo } from "./overviewController";
+import { AuthContext } from "../../../store/auth-context";
+import { useFonts } from "expo-font";
 
-const CircularUserRequests = () => {
-  //   const [refresh, setRefresh] = useState(true);
-  //   useEffect(() => {
-  //     setRefresh(!refresh);
-  //   }, []);
+const CircularUserRequests = ({ refreshing }) => {
+  const authCtx = useContext(AuthContext);
+  const [token, setToken] = useState(authCtx.token || null);
+  const [info, setInfo] = useState();
+  const [totalRequests, setTotalRequests] = useState(0);
+  const [totalReservations, setTotalReservations] = useState(0);
+  const [score, setScore] = useState(0);
+  // console.log({ refreshing });
+  useEffect(() => {
+    const getInfo = async () => {
+      let res = await getOverviewInfo(token);
+      setInfo(res);
+    };
+    if (token) {
+      getInfo();
+    }
+  }, [token, refreshing]);
+
+  useEffect(() => {
+    if (info) {
+      setTotalRequests(info.res.total_requests);
+      setTotalReservations(info.res.total_reservations);
+      setScore(totalRequests * totalReservations);
+    }
+  }, [info, totalRequests]);
+
+  //=============Font==========
+  const [loaded] = useFonts({
+    montserratBold: require("../../../assets/fonts/Montserrat-ExtraBold.ttf"),
+  });
+  if (!loaded) {
+    return null;
+  }
+  //=================
 
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollContainer}>
         <View style={styles.upperTxtWrapper}>
           <Ionicons name="trophy" size={24} color={Colors.primary500} />
-          <Text style={styles.upperTxt}>Loyalty Balance</Text>
+          <Text style={styles.upperTxt}>Wheels of Fortune</Text>
         </View>
         <View style={styles.circularOuterContainer}>
           <View style={[styles.circularContainer, styles.left]}>
             <CircularProgress
-              value={58}
+              value={totalRequests}
               initialValue={0}
               radius={60}
               duration={3000}
@@ -28,6 +60,7 @@ const CircularUserRequests = () => {
               progressValueStyle={{
                 fontWeight: "500",
                 color: Colors.background500,
+                fontFamily: "montserratBold",
               }}
               activeStrokeColor={Colors.primary500}
               maxValue={65}
@@ -39,7 +72,7 @@ const CircularUserRequests = () => {
 
           <View style={[styles.circularContainer, styles.right]}>
             <CircularProgress
-              value={120}
+              value={totalReservations}
               maxValue={180}
               radius={80}
               progressValueColor={Colors.earning500}
@@ -47,6 +80,7 @@ const CircularUserRequests = () => {
               progressValueStyle={{
                 fontWeight: "500",
                 color: Colors.background500,
+                fontFamily: "montserratBold",
               }}
               duration={2000}
               inActiveStrokeColor={Colors.primary500}
@@ -59,9 +93,9 @@ const CircularUserRequests = () => {
           </View>
         </View>
         <View>
-          <Text style={styles.bigTitle}>1240pts</Text>
+          <Text style={styles.bigTitle}>{score}pts</Text>
           <Text style={styles.smallTitle}>
-            1200 points till your next reward
+            1200 points till your next reward!
           </Text>
         </View>
       </ScrollView>
@@ -94,7 +128,9 @@ const styles = StyleSheet.create({
   },
   upperTxtWrapper: {
     flexDirection: "row",
-    margin: 15,
+    // margin: 15,
+    marginTop: 25,
+    marginLeft: 10,
   },
   upperTxt: {
     marginLeft: 5,
@@ -117,8 +153,8 @@ const styles = StyleSheet.create({
   },
   bigTitle: {
     fontSize: 40,
-    fontWeight: "800",
     color: Colors.earning500,
+    fontFamily: "montserratBold",
   },
   smallTitle: {
     fontWeight: "600",
