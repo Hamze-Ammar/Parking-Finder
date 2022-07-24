b53c134b389676f1f310e777400376ba0a598b53
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-24 17:41:29 +0300

    RJS | Defined Routes

--- frontend/reactjs/src/App.js
+++ frontend/reactjs/src/App.js
@@ -1,9 +1,46 @@
+import { useContext, useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/landingPage/LandingPage";
+import Login from "./pages/login/Login";
+import { AuthContext } from "./store/AuthContext";
 
 function App() {
-  return <div className="App">
-    <LandingPage/>
-  </div>;
+  const AuthCtx = useContext(AuthContext);
+
+  useEffect(() => {
+    function fetchToken() {
+      const storedToken = localStorage.getItem("token");
+      if (storedToken) {
+        AuthCtx.authenticate(storedToken);
+      }
+    }
+    fetchToken();
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <LandingPage />
+              </>
+            }
+          ></Route>
+          <Route
+            path="/login"
+            element={
+              <>
+                <Login />
+              </>
+            }
+          ></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
