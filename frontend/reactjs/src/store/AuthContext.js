b47c407635d215be9c4641f840269a1f96915184
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-24 17:42:37 +0300

    RJS | Added AuthContext

---
+++ frontend/reactjs/src/store/AuthContext.js
@@ -0,0 +1,33 @@
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext({
+  token: "",
+  isAuthenticated: false,
+  authenticate: (token) => {},
+  logout: () => {},
+});
+
+function AuthContextProvider({ children }) {
+  const [authToken, setAuthToken] = useState();
+
+  function authenticate(token) {
+    setAuthToken(token);
+    localStorage.setItem("token", token);
+  }
+
+  function logout() {
+    setAuthToken(null);
+    localStorage.removeItem("token");
+  }
+
+  const value = {
+    token: authToken,
+    isAuthenticated: !!authToken,
+    authenticate: authenticate,
+    logout: logout,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+
+export default AuthContextProvider;
