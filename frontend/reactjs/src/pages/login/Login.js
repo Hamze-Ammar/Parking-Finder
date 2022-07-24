fef347af4a703a99e432afc56f7103c8d9f9eaea
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-25 01:57:39 +0300

    RJS | Applied Spinner to Login

--- frontend/reactjs/src/pages/login/Login.js
+++ frontend/reactjs/src/pages/login/Login.js
@@ -7,6 +7,7 @@ import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
 import { URL } from "../../constant/backend";
 import { AuthContext } from "../../store/AuthContext";
+import SpinnerProgress from "../../components/circularProgress/SpinnerProgress";
 
 export default function Login() {
   const AuthCtx = useContext(AuthContext);
@@ -15,6 +16,7 @@ export default function Login() {
   const [password, setPassword] = useState("");
   const [userType, setUserType] = useState("");
   const [token, setToken] = useState("");
+  const [isLoading, setIsloading] = useState(false);
   let navigate = useNavigate();
   //show password
   const [passwordDisplay, setPasswordDisplay] = useState(false);
@@ -36,9 +38,10 @@ export default function Login() {
       token && AuthCtx.authenticate(token);
 
       //redirect user
-      alert("You are now logged in");
+      //   alert("You are now logged in");
       navigate("/");
     }
+    setIsloading(false);
   };
 
   //Add Data to Backend on Submit
@@ -48,6 +51,7 @@ export default function Login() {
       alert("Please fill all fields!");
       return;
     }
+    setIsloading(true);
     loginUser({ email, password });
     setEmail("");
     setPassword("");
@@ -66,82 +70,85 @@ export default function Login() {
   };
 
   return (
-    <div className={"loginContainer"}>
-      <span
-        className="close"
-        onClick={() => {
-          navigate("/");
-        }}
-      >
-        &times;
-      </span>
-      <h1 style={{ textAlign: "center" }}>Sign In</h1>
-      <hr />
+    <>
+      {isLoading && <SpinnerProgress />}
+      <div className={"loginContainer"}>
+        <span
+          className="close"
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          &times;
+        </span>
+        <h1 style={{ textAlign: "center" }}>Sign In</h1>
+        <hr />
 
-      <form onSubmit={onSubmit}>
-        <div className={"containerLogin"}>
-          <label htmlFor="email">
-            <b>Email</b>
-          </label>
-          <input
-            type="text"
-            placeholder="Enter Email"
-            name="email"
-            value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-            required
-          />
-
-          <label htmlFor="psw">
-            <b>Password</b>
-          </label>
-          <div className="password-field">
+        <form onSubmit={onSubmit}>
+          <div className={"containerLogin"}>
+            <label htmlFor="email">
+              <b>Email</b>
+            </label>
             <input
-              id="id_password"
-              type="password"
-              placeholder="Enter Password"
-              name="psw"
-              value={password}
+              type="text"
+              placeholder="Enter Email"
+              name="email"
+              value={email}
               onChange={(e) => {
-                setPassword(e.target.value);
+                setEmail(e.target.value);
               }}
               required
             />
-            {!passwordDisplay && (
-              <span className="eye" onClick={showPassword}>
-                <FaEye />
-              </span>
-            )}
-            {passwordDisplay && (
-              <span className="eye" onClick={showPassword}>
-                <FaEyeSlash />
-              </span>
-            )}
-          </div>
-          <button className="formBtn" type="submit">
-            Login
-          </button>
-          <label>
-            <input type="checkbox" name="remember" /> Remember me
-          </label>
-        </div>
 
-        <div className={"containerLogin"}>
-          <Link to="/">
-            <button className="formBtn cancelbtnn" type="button">
-              Cancel
+            <label htmlFor="psw">
+              <b>Password</b>
+            </label>
+            <div className="password-field">
+              <input
+                id="id_password"
+                type="password"
+                placeholder="Enter Password"
+                name="psw"
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                required
+              />
+              {!passwordDisplay && (
+                <span className="eye" onClick={showPassword}>
+                  <FaEye />
+                </span>
+              )}
+              {passwordDisplay && (
+                <span className="eye" onClick={showPassword}>
+                  <FaEyeSlash />
+                </span>
+              )}
+            </div>
+            <button className="formBtn" type="submit">
+              Login
             </button>
-          </Link>
-          <span className={"psw"}>
-            Don't have an account?{" "}
-            <Link to="/signUp">
-              <a href="#">Register</a>
+            <label>
+              <input type="checkbox" name="remember" /> Remember me
+            </label>
+          </div>
+
+          <div className={"containerLogin"}>
+            <Link to="/">
+              <button className="formBtn cancelbtnn" type="button">
+                Cancel
+              </button>
             </Link>
-          </span>
-        </div>
-      </form>
-    </div>
+            <span className={"psw"}>
+              Don't have an account?{" "}
+              <Link to="/signUp">
+                <a href="#">Register</a>
+              </Link>
+            </span>
+          </div>
+        </form>
+      </div>
+    </>
   );
 }
