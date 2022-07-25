96337a7867902375ce14385f81801e853157e199
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-25 13:14:10 +0300

    RJS | Added Map Component From pigeonMap

---
+++ frontend/reactjs/src/components/formParking/Map.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { Map, Marker } from "pigeon-maps";
+import FormContainer from "../../ui/FormContainer";
+
+export default function MyMap({ center, setPickedLocation }) {
+  const [defaultCenter, setDefaultCenter] = useState([
+    33.891163243590825, 35.505971217496125,
+  ]);
+  function handleClick(e) {
+    console.log(e.latLng);
+    setDefaultCenter([e.latLng[0], e.latLng[1]]);
+    setPickedLocation(defaultCenter);
+  }
+  return (
+    <FormContainer>
+      <Map
+        // height={100}
+        // width={100}
+        defaultCenter={defaultCenter}
+        defaultZoom={14}
+        onClick={handleClick}
+      >
+        <Marker width={50} anchor={defaultCenter} />
+      </Map>
+    </FormContainer>
+  );
+}
