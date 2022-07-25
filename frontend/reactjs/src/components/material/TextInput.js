d6424bc5ac7660e69acd118aa4f39de93cd4fbb5
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-25 13:11:50 +0300

    RJS | Updated TextInput Component

--- frontend/reactjs/src/components/material/TextInput.js
+++ frontend/reactjs/src/components/material/TextInput.js
@@ -3,23 +3,45 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 
-export default function TextInput({ title, setInput, type }) {
+export default function TextInput({
+  title,
+  setInput,
+  type,
+  disabled,
+  userInput,
+  inputProps,
+}) {
+  function handleInput(e) {
+    e.preventDefault();
+    if (inputProps) {
+      setInput(parseInt(e.target.value));
+    } else {
+      setInput(e.target.value);
+    }
+  }
   return (
     <Box
+      // fullWidth
       component="form"
-      sx={{
-        "& > :not(style)": { m: 1, width: "25ch" },
-      }}
+      style={{ width: "100%", height: "100%" }}
+      // sx={{
+      //   "& > :not(style)": { m: 1, width: "100%", height: "100%" },
+      // }}
       noValidate
       autoComplete="off"
     >
       <TextField
-        onChange={(e) => setInput(e.target.value)}
+        onKeyPress={(e) => e.key === "Enter" && e.preventDefault()}
+        onChange={handleInput}
         id="outlined-basic"
         label={title}
         variant="outlined"
         type={type && type}
         required
+        disabled={disabled}
+        style={{ width: "100%", height: "100%" }}
+        value={userInput}
+        inputProps={inputProps}
       />
     </Box>
   );
