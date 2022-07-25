e9b77115aae744872c8206cc188338f800953462
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-25 21:51:45 +0300

    RJS | Added registerParkingController

---
+++ frontend/reactjs/src/pages/registerParking/registerParkingController.js
@@ -0,0 +1,27 @@
+import { URL } from "../../constant/backend";
+
+export const saveNewParkingToServer = async (token, info) => {
+  console.log(URL);
+  console.log(token);
+  console.log(info);
+  if (!URL || !token || !info) {
+    return "Missing Values";
+  }
+  try {
+    const res = await fetch(`${URL}/user/becomePartner`, {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + token,
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(info),
+    });
+    const data = await res.json();
+    console.log(data.status)
+      if (data?.status){
+        return data.status;
+      }
+  } catch (err) {
+    console.log(err);
+  }
+};
