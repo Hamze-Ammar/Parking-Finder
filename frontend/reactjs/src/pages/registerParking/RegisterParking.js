b5ce2afb1a3d01e0c1674a7075186162cfbee84a
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-25 13:15:56 +0300

    RJS | Updated return in RegisterParking

--- frontend/reactjs/src/pages/registerParking/RegisterParking.js
+++ frontend/reactjs/src/pages/registerParking/RegisterParking.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { createUseStyles } from "react-jss";
 import RegisterForm from "../../components/formParking/RegisterForm";
+import MyMap from "../../components/formParking/Map";
 
 const RegisterParking = () => {
   const classes = useStyles();
+  const [pickedLocation, setPickedLocation] = useState();
 
   return (
-    <div style={classes.container}>
-      <RegisterForm />
+    <div className={classes.container}>
+      <RegisterForm pickedLocation={pickedLocation}/>
+      <MyMap setPickedLocation={setPickedLocation}  />
     </div>
   );
 };
 
 export default RegisterParking;
 
-const useStyles = createUseStyles({});
+const useStyles = createUseStyles({
+  container: {
+    height: "100vh",
+    margin: "auto",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: "20px",
+    flexWrap: "wrap",
+    backgroundColor: "red",
+  },
+});
