e2ba61c395cf1fafec59e0ee6c3ea050187c51c0
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-26 01:03:19 +0300

    RJS | Added loginController

---
+++ frontend/reactjs/src/pages/login/loginController.js
@@ -0,0 +1,21 @@
+import { URL } from "../../constant/backend";
+
+export const checkForPendingRequest = async (token) => {
+  if (!token) return;
+  try {
+    const res = await fetch(`${URL}/user/hasPendingRequest`, {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + token,
+        "Content-type": "application/json",
+      },
+    });
+    const data = await res.json();
+    console.log(data.status);
+    if (data?.status) {
+      return data.res;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
