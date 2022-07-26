cd2504896ab6b6c359b2321bb6260d52ea694e3e
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-27 02:22:29 +0300

    RJS | Added ComingSoon Component

---
+++ frontend/reactjs/src/pages/admin/ComingSoon.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { createUseStyles } from "react-jss";
+import { Colors } from "../../constant/color";
+
+const ComingSoon = () => {
+  const classes = useStyles();
+  return <div className={classes.container}>&lt;Coming Soon/&gt;</div>;
+};
+
+export default ComingSoon;
+
+const useStyles = createUseStyles({
+  container: {
+    color: Colors.secondary500,
+    fontFamily: "Montserrat",
+    fontSize: "25px",
+    fontWeight: "700",
+    
+    marginTop: "300px",
+    textAlign: "center",
+  },
+});
