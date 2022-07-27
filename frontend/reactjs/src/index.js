1228eb5ec9c8591a610e65c7d62a72112c53d106
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-27 09:54:52 +0300

    RJS | Added AdminPanelProvider To index.js

--- frontend/reactjs/src/index.js
+++ frontend/reactjs/src/index.js
@@ -4,15 +4,17 @@ import App from "./App";
 import "./index.css";
 import "./App.css";
 import AuthContextProvider from "./store/AuthContext";
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
+import AdminPanelContextProvider from "./store/AdminPanelContext";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AuthContextProvider>
-    <App />
+    <AdminPanelContextProvider>
+      <App />
+    </AdminPanelContextProvider>
   </AuthContextProvider>
 );
