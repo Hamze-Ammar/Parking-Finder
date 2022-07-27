162c90b46e4584bb7895486cbc89f1f72fcdf5b0
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-27 18:05:14 +0300

    RJS | Added OverView Component

---
+++ frontend/reactjs/src/pages/admin/overview/OverView.js
@@ -0,0 +1,15 @@
+import React from "react";
+import OverViewHeader from "./OverViewHeader";
+
+const OverView = () => {
+  return (
+    <div>
+      <div>
+        <OverViewHeader />
+      </div>
+      OverView
+    </div>
+  );
+};
+
+export default OverView;
