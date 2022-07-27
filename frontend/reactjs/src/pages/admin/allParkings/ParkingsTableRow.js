0e2331e71b554b53e071bc45d2f5cd31c5cdf350
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-27 15:46:39 +0300

    RJS | Added ParkingsTableRow

---
+++ frontend/reactjs/src/pages/admin/allParkings/ParkingsTableRow.js
@@ -0,0 +1,88 @@
+import React, { useContext, useState, useEffect } from "react";
+import { createUseStyles } from "react-jss";
+import { Colors } from "../../../constant/color";
+import { AuthContext } from "../../../store/AuthContext";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import { deleteParking } from "./allParkingsController";
+import { AdminPanelContext } from "../../../store/AdminPanelContext";
+
+const ParkingsTableRow = ({ parking, refreshTable, setOpen }) => {
+  const classes = useStyles();
+  const AuthCtx = useContext(AuthContext);
+  const AdminCtx = useContext(AdminPanelContext);
+
+  let status;
+  let statusColor;
+  if (parking?.is_approved == 1) {
+    status = "Active";
+    statusColor = classes.accepted;
+  } else if (parking?.is_approved == -1) {
+    status = "Declined";
+    statusColor = classes.declined;
+  } else {
+    status = "Pending";
+    statusColor = classes.pending;
+  }
+
+  const handleDelete = async () => {
+    deleteParking(AuthCtx.token, parking.id).then((response) => {
+      //   console.log(response);
+      setOpen(response);
+    });
+    if (parking.is_approved == 0) {
+      AdminCtx.updatePendingRequests();
+    }
+    refreshTable(parking.id);
+  };
+
+  return (
+    <>
+      <tr>
+        <td>{parking.id}</td>
+        <td>{parking.name}</td>
+        <td>{parking.total_slots}</td>
+        <td>{parking.num_of_active_slots}</td>
+        <td>{parking.city_name}</td>
+        <td>{parking.country_name}</td>
+        <td>{parking.user_name}</td>
+        <td>{parking.user.email}</td>
+        <td className={statusColor}>{status}</td>
+        <td style={{ textAlign: "right" }}>
+          <DeleteForeverIcon
+            onClick={handleDelete}
+            className={[classes.icon, classes.decline]}
+          />
+        </td>
+      </tr>
+    </>
+  );
+};
+
+export default ParkingsTableRow;
+
+const useStyles = createUseStyles({
+  icon: {
+    marginLeft: "15px",
+    cursor: "pointer",
+    borderRadius: "10px",
+    "&:hover": {
+      color: "rgba(255,255,255,0.8)",
+      backgroundColor: Colors.error200,
+    },
+  },
+  decline: {
+    color: Colors.error500,
+  },
+  accepted: {
+    backgroundColor: Colors.accepted,
+    color: Colors.fontSecondary,
+  },
+  declined: {
+    backgroundColor: Colors.declined,
+    color: Colors.fontSecondary,
+  },
+  pending: {
+    backgroundColor: Colors.pending,
+    color: Colors.fontSecondary,
+  },
+});
