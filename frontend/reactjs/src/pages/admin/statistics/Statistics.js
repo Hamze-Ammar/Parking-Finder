6d6e3a8160138780b71acd9d8f27b969a085bb7d
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-28 15:57:10 +0300

    RJS | Added Statistics To  Admin Dashboard

---
+++ frontend/reactjs/src/pages/admin/statistics/Statistics.js
@@ -0,0 +1,31 @@
+import React from "react";
+import WeeklyChart from "./WeeklyChart";
+import { createUseStyles } from "react-jss";
+
+const Statistics = () => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <div className={classes.tableContainer}>
+        <h1>Weekly Overview</h1>
+        <div className={classes.chartContainer}>
+          <WeeklyChart />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Statistics;
+
+const useStyles = createUseStyles({
+  tableContainer: {
+    fontSize: "14px",
+    fontFamily: "Montserrat",
+  },
+  chartContainer: {
+    marginTop: "40px",
+    // height: "100%",
+  },
+});
