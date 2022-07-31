8f119b77e579449383d60bdcd137c49c084ace93
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-08-01 00:52:23 +0300

    RN | Added PopupReservation

--- frontend/ReactNative/components/parkingPage/ParkingView.js
+++ frontend/ReactNative/components/parkingPage/ParkingView.js
@@ -1,17 +1,16 @@
 import { useContext } from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, Pressable } from "react-native";
 import React, { useEffect, useState } from "react";
-import Slot from "../../components/parkingPage/Slot";
-import { Button } from "../../ui/Button";
-import { Colors } from "../../constants/styles";
 import { AuthContext } from "../../store/auth-context";
 import { ParkingContext } from "../../store/parkingContext";
-
+import PopupReservation from "./PopupReservation";
 import { getParkingById } from "./parkingController";
 import ParkingHeader from "./ParkingHeader";
 import NotFound from "../../util/NotFound";
 import Slots from "./Slots";
 import LoadingOverlay from "../../ui/LoadingOverlay";
+import { Colors, dimensions } from "../../constants/styles";
+import { sendReservation } from "./parkingController";
 
 const ParkingView = ({
   cityName,
@@ -33,18 +32,13 @@ const ParkingView = ({
   const [loading, setLoading] = useState(false);
   const [refresh, setRefresh] = useState(false);
   const [showTimer, setShowTimer] = useState(false);
-  // console.log({ parking });
-
-  // useEffect(() => {
-  //   console.log("hello, world");
-  //   fetchData();
-  // }, [parkingCtx.refresh]);
+  const [showPopupOptions, setShowPopupOptions] = useState(false);
+  const [reservationDuration, setReservationDuration] = useState();
+  const [reservedSlotID, setReservedSlotID] = useState();
 
   useEffect(() => {
     const fetchData = async () => {
-      // console.log({ parkingId });
       const res = await getParkingById(parkingId, token);
-      // console.log("res                  .....       ", res);
       if (res) {
         setParking(res); // To be passed to the child
         setMyParking(res); // To be returned to the parent
@@ -75,6 +69,15 @@ const ParkingView = ({
     }
   }, [parking]);
 
+  // Handle set Timer and reservation
+  const handleSetTimer = (duration) => {
+    console.log(duration);
+    setShowPopupOptions(false);
+    setReservationDuration(duration);
+    setShowTimer(true);
+    sendReservation(reservedSlotID, duration, token);
+  };
+
   if (not_found) {
     return <NotFound />;
   }
@@ -103,8 +106,18 @@ const ParkingView = ({
             setShowTimer={(boolean) => {
               setShowTimer(boolean);
             }}
+            reservationDuration={reservationDuration}
             showTimer={showTimer}
+            setShowPopupOptions={setShowPopupOptions}
+            setReservedSlotID={setReservedSlotID}
           />
+        )}
+        {showPopupOptions && (
+          <View style={styles.popupOuterContainer}>
+            <View style={styles.popupContainer}>
+              <PopupReservation onPress={handleSetTimer} />
+            </View>
+          </View>
         )}
       </View>
     </>
@@ -113,4 +126,31 @@ const ParkingView = ({
 
 export default ParkingView;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  popupOuterContainer: {
+    position: "absolute",
+    width: dimensions.width,
+    height: dimensions.height,
+    top: 0,
+    backgroundColor: "rgba(255, 255, 255, 0.3)",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  popupContainer: {
+    width: "80%",
+    height: "45%",
+    borderRadius: 15,
+    padding: 15,
+    backgroundColor: Colors.background200,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 2,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4.84,
+
+    elevation: 5,
+  },
+});
