983228212ee6b98ef27af7c280551d201caa7b55
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-07-31 19:01:41 +0300

    Laravel | Added UserSeeder

--- backend-Laravel/database/seeders/DatabaseSeeder.php
+++ backend-Laravel/database/seeders/DatabaseSeeder.php
@@ -13,11 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-
-        // \App\Models\User::factory(10)->create();
-        // $this->call(UserSeeder::class);
+        $this->call(UserSeeder::class);
         $this->call(CountrySeeder::class);
         $this->call(CitySeeder::class);
         $this->call(UserTypeSeeder::class);
+        $this->call(ParkingSeeder::class);
+        $this->call(SlotSeeder::class);
     }
 }
--- backend-Laravel/database/seeders/UserSeeder.php
+++ backend-Laravel/database/seeders/UserSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use App\Models\User;
 
 
@@ -15,9 +16,16 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
-        User::factory()
-        ->count(10)
-        ->create();
+        DB::table('users')->delete();
+
+        $Records = [
+            ['id'=>1, 'name'=>'user',    'email'=>'user@user.com',       'password'=>'$2y$10$WE5tIVBeIvueu3WkVzhGqeERfB2T8WNnH9eOY51ilkvzkWzJRjW6W', 'user_type_id'=>'1'],
+            ['id'=>2, 'name'=>'owner10', 'email'=>'owner10@owner.com',   'password'=>'$2y$10$WE5tIVBeIvueu3WkVzhGqeERfB2T8WNnH9eOY51ilkvzkWzJRjW6W', 'user_type_id'=>'2'],
+            ['id'=>3, 'name'=>'owner20', 'email'=>'owner20@owner.com',   'password'=>'$2y$10$WE5tIVBeIvueu3WkVzhGqeERfB2T8WNnH9eOY51ilkvzkWzJRjW6W', 'user_type_id'=>'2'],
+            ['id'=>4, 'name'=>'owner30', 'email'=>'owner30@owner.com',   'password'=>'$2y$10$WE5tIVBeIvueu3WkVzhGqeERfB2T8WNnH9eOY51ilkvzkWzJRjW6W', 'user_type_id'=>'2'],
+            ['id'=>5, 'name'=>'admin',   'email'=>'admin@admin.com',     'password'=>'$2y$10$WE5tIVBeIvueu3WkVzhGqeERfB2T8WNnH9eOY51ilkvzkWzJRjW6W', 'user_type_id'=>'3'],
+
+        ];
+        User::insert($Records);
     }
 }
