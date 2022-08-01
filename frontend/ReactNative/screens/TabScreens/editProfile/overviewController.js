d0b1e1d3495b4cb29408f00e149f37f65258c48c
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-08-01 17:18:03 +0300

    RN | Added API Calls Functions overviewController

--- frontend/ReactNative/screens/TabScreens/editProfile/overviewController.js
+++ frontend/ReactNative/screens/TabScreens/editProfile/overviewController.js
@@ -13,8 +13,52 @@ export const getOverviewInfo = async (token) => {
   });
   const data = await res.json();
   if (data.status === "Success") {
-    // console.log(data.res);
     return data;
+  } else {
+    return false;
+  }
+};
+
+export const getUserProfile = async (token) => {
+  if (!token) {
+    return;
+  }
+  const res = await fetch(`${URL}/user/getUserProfile`, {
+    method: "GET",
+    headers: {
+      Authorization: "Bearer " + token,
+      "Content-type": "application/json",
+    },
+  });
+  const data = await res.json();
+  if (data.status === "Success") {
+    if (data?.res) {
+      // console.log(data.res);
+      return data?.res;
+    }
+  } else {
+    return false;
+  }
+};
+
+export const editProfile = async (token, credentials) => {
+  if (!token) {
+    return;
+  }
+  const res = await fetch(`${URL}/user/editProfile`, {
+    method: "PUT",
+    headers: {
+      Authorization: "Bearer " + token,
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  });
+  const data = await res.json();
+  if (data.status === "Success") {
+    // console.log(data);
+     if(data?.res){
+      return(data.res)
+    };
   } else {
     return false;
   }
