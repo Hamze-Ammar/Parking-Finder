5a2bab0ef919e944249a6ab91c3fe8195b3d59cb
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-08-01 10:24:49 +0300

    RJS | Updated OverViewHeader Component

--- frontend/reactjs/src/pages/admin/overview/OverViewHeader.js
+++ frontend/reactjs/src/pages/admin/overview/OverViewHeader.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { createUseStyles } from "react-jss";
 import { Colors } from "../../../constant/color";
 import { FiUsers } from "react-icons/fi";
@@ -6,26 +6,53 @@ import { AiOutlineCar } from "react-icons/ai";
 import { RiParkingLine } from "react-icons/ri";
 import { FaRegFlag } from "react-icons/fa";
 import { FaCity } from "react-icons/fa";
-
 import OverviewSnippet from "./OverviewSnippet";
+import { AuthContext } from "../../../store/AuthContext";
+import { getOverviewHeaderInfo } from "./overviewController";
 
 const OverViewHeader = () => {
+  const authCtx = useContext(AuthContext);
+  // authCtx.token
   const classes = useStyles();
+  const [info, setInfo] = useState({});
+  console.log(info);
+  useEffect(() => {
+    const fetchInfo = async (token) => {
+      setInfo(await getOverviewHeaderInfo(token));
+    };
+    fetchInfo(authCtx.token);
+  }, []);
 
   return (
     <>
       <div className={classes.container}>
         <div>Overview</div>
         <div className={classes.infoContainer}>
-          <OverviewSnippet icon={<FiUsers />} title="Users" info="239" />
+          <OverviewSnippet
+            icon={<FiUsers />}
+            title="Users"
+            info={info?.num_of_users}
+          />
           <OverviewSnippet
             icon={<RiParkingLine />}
             title="Parkings"
-            info="39"
+            info={info?.num_of_parkings}
           />
-          <OverviewSnippet icon={<AiOutlineCar />} title="Spots" info="439" />
-          <OverviewSnippet icon={<FaRegFlag />} title="Countries" info="4" />
-          <OverviewSnippet icon={<FaCity />} title="Cities" info="14" />
+          <OverviewSnippet
+            icon={<AiOutlineCar />}
+            title="Spots"
+            info={info?.num_of_slots}
+          />
+          <OverviewSnippet
+            icon={<FaRegFlag />}
+            title="Countries"
+            info={info?.num_of_countries}
+          />
+          <OverviewSnippet
+            icon={<FaCity />}
+            title="Cities"
+            info={info?.num_of_cities}
+          />
         </div>
       </div>
     </>
