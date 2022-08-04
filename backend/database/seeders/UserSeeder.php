78309274a10be2d98fde258aa04ca0bf9c6af6da
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-08-04 19:50:12 +0300

    Laravel | Fixed UserSeeder

--- backend/database/seeders/UserSeeder.php
+++ backend/database/seeders/UserSeeder.php
@@ -16,14 +16,14 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->delete();
+        // DB::table('users')->delete();
 
         $Records = [
-            ['id'=>1, 'name'=>'user',    'email'=>'user@user.com',       'password'=>bcrypt('123123') , 'user_type_id'=>'1', 'created_at'=>date("Y-m-d H:i:s")],
-            ['id'=>2, 'name'=>'owner10', 'email'=>'owner10@owner.com',   'password'=>bcrypt('123123') , 'user_type_id'=>'2', 'created_at'=>date("Y-m-d H:i:s")],
-            ['id'=>3, 'name'=>'owner20', 'email'=>'owner20@owner.com',   'password'=>bcrypt('123123') , 'user_type_id'=>'2', 'created_at'=>date("Y-m-d H:i:s")],
-            ['id'=>4, 'name'=>'owner30', 'email'=>'owner30@owner.com',   'password'=>bcrypt('123123') , 'user_type_id'=>'2', 'created_at'=>date("Y-m-d H:i:s")],
-            ['id'=>5, 'name'=>'admin',   'email'=>'admin@admin.com',     'password'=>bcrypt('123123') , 'user_type_id'=>'3', 'created_at'=>date("Y-m-d H:i:s")],
+            ['name'=>'user',    'email'=>'user@user.com',       'password'=>bcrypt('123123') , 'user_type_id'=>'1', 'created_at'=>date("Y-m-d H:i:s")],
+            ['name'=>'owner10', 'email'=>'owner10@owner.com',   'password'=>bcrypt('123123') , 'user_type_id'=>'2', 'created_at'=>date("Y-m-d H:i:s")],
+            ['name'=>'owner20', 'email'=>'owner20@owner.com',   'password'=>bcrypt('123123') , 'user_type_id'=>'2', 'created_at'=>date("Y-m-d H:i:s")],
+            ['name'=>'owner30', 'email'=>'owner30@owner.com',   'password'=>bcrypt('123123') , 'user_type_id'=>'2', 'created_at'=>date("Y-m-d H:i:s")],
+            ['name'=>'admin',   'email'=>'admin@admin.com',     'password'=>bcrypt('123123') , 'user_type_id'=>'3', 'created_at'=>date("Y-m-d H:i:s")],
 
         ];
         User::insert($Records);
